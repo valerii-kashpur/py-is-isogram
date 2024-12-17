2d960548f20a4282497c532916e532e7cf1ae5c7
Valerii Kashpur <kashpur.v.f@gmail.com>
2024-12-17 18:46:54 +0200

    isogram tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        pytest.param("playgrounds", True,
+                     id="should return True if no repeating letters"),
+        pytest.param("look", False,
+                     id="return False if some letters repeat"),
+        pytest.param("Adam", False,
+                     id="function should be case-insensitive"),
+        pytest.param("", True,
+                     id="empty string is an isogram"),
+    ]
+)
+def test_is_isogram(test_input: str, expected: bool) -> None:
+    assert is_isogram(test_input) == expected
